17a52739dfb48ba95b967a54f5284ee343dad516
prathameshfuke <prathameshfuke@users.noreply.github.com>
2026-01-28 16:31:38 +0530

    chore: set default theme to light

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <TooltipProvider>
         <Toaster />
         <Sonner />
